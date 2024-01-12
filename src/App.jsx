8eee96a645c57cdd77d535ba0b80dbada258a811
sanskar-23 <skapoor_be20@thapar.edu>
2024-01-12 21:38:00 +0530

    Added HeroBanner Component

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,20 @@ function App() {
   const { url } = useSelector((state) => state.home);
   console.log(url);
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular")
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration")
       .then((res) => {
         console.log(res.results);
-        dispatch(getApiconfiguration(res));
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
+        };
+
+        dispatch(getApiconfiguration(url));
       })
       .catch((error) => {
         console.log(error);
@@ -32,7 +38,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
@@ -40,7 +46,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
