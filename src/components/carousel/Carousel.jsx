253783cc9178299240815cb43487c28b7bcabfdb
sanskar-23 <skapoor_be20@thapar.edu>
2024-01-19 16:51:58 +0530

    Created The circle Rating

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -11,6 +11,7 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import "./style.scss";
+import CircleRating from "../circleRating/CircleRating";
 
 const Carousel = ({ data, loading }) => {
   const carouselContainer = useRef();
@@ -19,6 +20,18 @@ const Carousel = ({ data, loading }) => {
 
   const navigation = (dir) => {
     console.log(dir);
+  };
+
+  const skItem = () => {
+    return (
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
+        </div>
+      </div>
+    );
   };
   return (
     <div className="carousel">
@@ -41,13 +54,26 @@ const Carousel = ({ data, loading }) => {
                 <div className="carouselItem" key={item.id}>
                   <div className="posterBlock">
                     <Img src={posterUrl} />
+                    <CircleRating rating={item.vote_average.toFixed(1)} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{item.title || item.name}</span>
+                    <span className="date">
+                      {dayjs(item.release_date).format("MMM D, YYYY")}
+                    </span>
                   </div>
                 </div>
               );
             })}
           </div>
         ) : (
-          <span>Loading...</span>
+          <div className="loadingSkeleton">
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+          </div>
         )}
       </ContentWrapper>
     </div>
